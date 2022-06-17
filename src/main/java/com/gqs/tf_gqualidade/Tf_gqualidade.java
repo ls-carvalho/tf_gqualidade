ce7d9e1c5f40bd0fa91042ee2e5d084e09ab3a2b
ls-carvalho <lucas-carvalho13@outlook.com>
2022-06-17 16:42:59 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/gqs/tf_gqualidade/Tf_gqualidade.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.gqs.tf_gqualidade;
+
+/**
+ *
+ * @author Lucas dos Santos Carvalho
+ */
+public class Tf_gqualidade {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
